448eb32c45b44943b1f69fa3c2c6844d8b287fd4
daviecud <davidwcalderwood@hotmail.com>
2019-08-26 01:04:46 +0100

    migrate to AndroidX

--- app/src/main/java/com/example/renfrewlive/LostFoundActivity.java
+++ app/src/main/java/com/example/renfrewlive/LostFoundActivity.java
@@ -2,9 +2,9 @@ package com.example.renfrewlive;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.Toast;
 
 import com.hitomi.cmlibrary.CircleMenu;
--- app/src/main/java/com/example/renfrewlive/MainActivity.java
+++ app/src/main/java/com/example/renfrewlive/MainActivity.java
@@ -10,17 +10,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.view.View;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.widget.Toolbar;
 
 
 import java.text.DateFormat;
